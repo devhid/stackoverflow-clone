e90bce7782ab012e5de5e80ab97d1308fa320e5c
devhid <devhid@users.noreply.github.com>
2019-03-26 07:30:22 -0400

    Updated gateway for localhost

--- fake_gateway/index.js
+++ fake_gateway/index.js
@@ -1,7 +1,6 @@
 /* library imports */
 const express = require('express');
 const request = require('request');
-const cookieParser = require('cookie-parser');
 
 /* internal imports */
 const servers = require('./servers');
@@ -14,7 +13,6 @@ const PORT = 5000;
 
 /* parse incoming requests data as json */
 app.use(express.json());
-app.use(cookieParser())
 
 /* all routes for stack overflow api */
 
@@ -30,19 +28,6 @@ app.post('/adduser', (req, res) => {
 
 app.post('/login', (req, res) => {
     request.post(servers.AUTHENTICATION + '/login', { "json":req.body }, (error, response, body) => {
-<<<<<<< HEAD
-        cookie = response.headers['set-cookie'][0].split('; ')[0].split('=');
-        res.cookie(cookie[0], cookie[1], {domain:"kellogs.cse356.compas.cs.stonybrook.edu", path: '/'});
-        res.set({
-            "Access-Control-Allow-Origin": "http://kellogs.cse356.compas.cs.stonybrook.edu",
-            "Access-Control-Allow-Credentials": true,
-            "Access-Control-Allow-Methods": ["GET", "POST"],
-            "Access-Control-Allow-Headers": ["Content-Type", "*"]
-        })
-        console.log(res);
-=======
-        console.log(response);
->>>>>>> 30a21d642a0929f6690fb2ac9f620f5ca2f974f7
         return res.json(body);
     });
 });
--- fake_gateway/servers.js
+++ fake_gateway/servers.js
@@ -1,7 +1,15 @@
+// module.exports = {
+//     AUTHENTICATION: 'http://64.190.90.243', // SkySilk
+//     REGISTRATION: 'http://130.245.171.197', // OpenStack
+//     EMAIL_VERIFICATION: 'http://207.148.20.88', // Vultr
+//     QA: 'http://140.82.9.197', // Vultr
+//     SEARCH: 'http://64.190.91.125' // SkySilk
+// }
+
 module.exports = {
-    AUTHENTICATION: 'http://64.190.90.243', // SkySilk
-    REGISTRATION: 'http://130.245.171.197', // OpenStack
-    EMAIL_VERIFICATION: 'http://207.148.20.88', // Vultr
-    QA: 'http://140.82.9.197', // Vultr
-    SEARCH: 'http://64.190.91.125' // SkySilk
+    AUTHENTICATION: 'http://localhost:3000', // SkySilk
+    REGISTRATION: 'http://localhost:3003', // OpenStack
+    EMAIL_VERIFICATION: 'http://localhost:3001', // Vultr
+    QA: 'http://localhost:3002', // Vultr
+    SEARCH: 'http://localhost:3004' // SkySilk
 }
