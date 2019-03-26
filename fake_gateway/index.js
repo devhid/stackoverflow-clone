ed60b1338020e538fbe330e35ed5a89ec304d2ef
devhid <devhid@users.noreply.github.com>
2019-03-26 03:25:49 -0400

    Added fake gateway to forward all client requests to appropriate services.

---
+++ fake_gateway/index.js
@@ -0,0 +1,71 @@
+/* library imports */
+const express = require('express');
+const request = require('request');
+
+/* internal imports */
+const servers = require('./servers');
+
+/* initialize express application */
+const app = express();
+
+/* port to run server on */
+const PORT = 8000;
+
+/* all routes for stack overflow api */
+
+app.post('/adduser', (req, res) => {
+    request.post(servers.REGISTRATION + '/adduser', { "form":req.body }, (error, response, body) => {
+        return res.json(JSON.parse(body));
+    });
+});
+
+app.post('/login', (req, res) => {
+    request.post(servers.AUTHENTICATION + '/login', { "form":req.body }, (error, response, body) => {
+        return res.json(JSON.parse(body));
+    });
+});
+
+app.post('/logout', (req, res) => {
+    request.post(servers.AUTHENTICATION + '/logout', { "form":req.body }, (error, response, body) => {
+        return res.json(JSON.parse(body));
+    });
+});
+
+app.post('/verify', (req, res) => {
+    request.post(servers.EMAIL_VERIFICATION + '/verify', { "form":req.body }, (error, response, body) => {
+        return res.json(JSON.parse(body));
+    });
+});
+
+app.post('/questions/add', (req, res) => {
+    request.post(servers.QA, '/questions/add', { "form":req.body }, (error, response, body) => {
+        return res.json(JSON.parse(body));
+    });
+});
+
+app.get('/questions/:qid', (req, res) => {
+    request.get(servers.QA + '/questions/' + req.params.qid, (error, response, body) => {
+        return res.json(JSON.parse(body));
+    });
+});
+
+app.post('/questions/:qid/answers/add', (req, res) => {
+    request.post(servers.QA + '/questions/' + req.params.qid + '/answers/add', { "form":req.body }, (error, response, body) => {
+        return res.json(JSON.parse(body));
+    });
+});
+
+app.get('/questions/:qid/answers', (req, res) => {
+    request.get(servers.QA + '/questions/' + req.params.qid + '/answers', (error, response, body) => {
+        return res.json(JSON.parse(body));
+    });
+});
+
+app.post('/search', (req, res) => {
+    request.post(servers.SEARCH + '/search', { "form": req.body }, (error, response, body) => {
+        return res.json(JSON.parse(body));
+    });
+});
+
+/* Start the server. */
+app.listen(PORT, () => console.log(`Server running on http://127.0.0.1:${PORT}`));
---
+++ fake_gateway/servers.js
@@ -0,0 +1,7 @@
+module.exports = {
+    AUTHENTICATION: 'http://64.190.90.243', // SkySilk
+    REGISTRATION: 'http://130.245.171.197', // OpenStack
+    EMAIL_VERIFICATION: 'http://207.148.20.88', // Vultr
+    QA: 'http://140.82.9.197', // Vultr
+    SEARCH: 'http://64.190.91.125' // SkySilk
+}
