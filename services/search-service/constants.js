64f528437dad8ea3e5098982e73ece20dbe3f824
Johnny So <jso123450@gmail.com>
2019-03-26 11:11:52 -0400

    Update constants.js

--- services/search-service/constants.js
+++ services/search-service/constants.js
@@ -1,5 +1,10 @@
+
+function getCurrentTime(){
+    return Date.now()/1000;
+}
+
 module.exports = {
-    DEFAULT_TIMESTAMP: Math.round(new Date().getTime() / 1000),
+    DEFAULT_TIMESTAMP: getCurrentTime,
     DEFAULT_LIMIT: 25,
     DEFAULT_MAX_LIMIT: 100,
     DEFAULT_ACCEPTED: false
