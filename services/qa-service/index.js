d177d1731e824ab32e97889ed9807d89ae235a18
Johnny So <jso123450@gmail.com>
2019-04-20 19:48:50 -0400

    CORS header

--- services/qa-service/index.js
+++ services/qa-service/index.js
@@ -45,6 +45,7 @@ app.use(function(req, res, next) {
   res.set('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.set('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
   res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.set('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
