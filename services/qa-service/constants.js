08d93dfca81906e093659c68eea16fc08a589c39
devhid <devhid@users.noreply.github.com>
2019-04-22 00:18:18 -0400

    changed contact point for cassandra in qa

--- services/qa-service/constants.js
+++ services/qa-service/constants.js
@@ -33,7 +33,7 @@ module.exports = {
     ERR_MEDIA_DELETE_FAILED: 'An error occurred while deleting the media.',
 
     CASSANDRA_OPTIONS: {
-        contactPoints: ["0.0.0.0:9160"], 
+        contactPoints: ["192.168.122.22"], 
         localDataCenter: 'datacenter1', 
         keyspace: "media_service",
         table: "imgs"
