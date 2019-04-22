f922995377d3cd32e17bc2f51d1bcf17493f42a5
Johnny So <jso123450@gmail.com>
2019-04-22 17:52:02 -0400

    Updated rabbit router constant

--- services/qa-service/constants.js
+++ services/qa-service/constants.js
@@ -9,7 +9,7 @@ module.exports = {
     STATUS_404: 404,    // not found
     STATUS_409: 409,    // conflict
 
-    AMQP_HOST: 'amqp://localhost',
+    AMQP_HOST: 'amqp://130.245.170.211',
 
     EXCHANGE: {
         TYPE: 'direct',
