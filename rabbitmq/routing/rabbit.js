ed68fd02e31caa432766ac596cead0caf764cbea
Andy Liang <andy.liang.1@stonybrook.edu>
2019-04-23 02:33:46 -0400

    Converted amqp callbacks to promises

--- rabbitmq/routing/rabbit.js
+++ rabbitmq/routing/rabbit.js
@@ -1,5 +1,6 @@
 /* library imports */
-const amqp = require('amqplib/callback_api');
+//const amqp = require('amqplib/callback_api');
+const amqp = require('amqplib');
 const uuidv4 = require('uuid/v4');
 
 /* internal imports */
@@ -12,7 +13,7 @@ let correlationId = null;
 var conn = null;
 var ch = null;
 try {
-    amqp.connect(constants.AMQP_HOST, function(error0, connection) {
+    /*amqp.connect(constants.AMQP_HOST, function(error0, connection) {
         if (error0) {
             throw error0;
         }
@@ -29,7 +30,11 @@ try {
                 console.log(`ok ${JSON.stringify(ok)}`);
             });
         });
-    });
+    });*/
+    conn = await amqp.connect(constants.AMQP_HOST);
+    ch = await conn.createChannel();
+    await ch.assertExchange(constants.EXCHANGE.NAME, constants.EXCHANGE.TYPE, constants.EXCHANGE.PROPERTIES);
+    console.log(`ok ${JSON.stringify(ok)}`);
 }
 catch (err){
     console.log(`[Rabbit] Failed to connect ${err}`);
@@ -40,17 +45,46 @@ catch (err){
  * @param {string} routing_key the routing key of the Queue (used as the routing key)
  * @param {Object} data data to encapsulate in a message
  */
+/*async function publishMessage(routing_key, data){
+    let dbResult = new DBResult();
+    return new Promise( (resolve,reject) => {
+        ch.assertQueue(constants.CALLBACK_QUEUE, constants.QUEUE.PROPERTIES, function(error2, q) {
+            if (error2) {
+                dbResult.status = constants.RMQ_ERROR;
+                dbResult.data = error2;
+                reject(dbResult);
+            }
+
+            correlationId = uuidv4();
+            console.log(` [x] Requesting ${JSON.stringify(data)}, corrId=${correlationId}`);
+
+            ch.publish(constants.EXCHANGE.NAME, 
+                routing_key, 
+                Buffer.from(JSON.stringify(data)), 
+                { correlationId: correlationId, replyTo: q.queue, persistent: true }
+            );
+
+            ch.consume(q.queue, (msg) => {
+                if (msg.properties.correlationId === correlationId){
+                    console.log(` [.] Got ${msg.content.toString()}, corrId=${correlationId}`);
+                    ch.ack(msg);
+                    dbResult.status = constants.RMQ_SUCCESS;
+                    dbResult.data = JSON.parse(msg.content.toString());
+                    resolve(dbResult);
+                    return;
+                }
+                console.log(` [.] Received corrId=${msg.properties.correlationId}, expected=${correlationId}`);
+            }, { noAck: false });
+        });
+    });
+}*/
+
 async function publishMessage(routing_key, data){
     let dbResult = new DBResult();
-    
-    ch.assertQueue(constants.CALLBACK_QUEUE, constants.QUEUE.PROPERTIES, function(error2, q) {
-        if (error2) {
-            dbResult.status = constants.RMQ_ERROR;
-            dbResult.data = error2;
-        }
 
-        correlationId = uuidv4();
-        console.log(` [x] Requesting ${JSON.stringify(data)}, corrId=${correlationId}`);
+    try {
+        await ch.assertQueue(constants.CALLBACK_QUEUE, constants.QUEUE.PROPERTIES);
+        correlationId = uuid4v();
 
         ch.publish(constants.EXCHANGE.NAME, 
             routing_key, 
@@ -58,18 +92,24 @@ async function publishMessage(routing_key, data){
             { correlationId: correlationId, replyTo: q.queue, persistent: true }
         );
 
-        ch.consume(q.queue, (msg) => {
-            if (msg.properties.correlationId === correlationId){
-                console.log(` [.] Got ${msg.content.toString()}, corrId=${correlationId}`);
-                ch.ack(msg);
-                dbResult.status = constants.RMQ_SUCCESS;
-                dbResult.data = JSON.parse(msg.content.toString());
-                return;
-            }
-            console.log(` [.] Received corrId=${msg.properties.correlationId}, expected=${correlationId}`);
-        }, { noAck: false });
-    });
+        let msg = await ch.consume(q.queue, { noAck: false });
+
+        if (msg.properties.correlationId === correlationId) {
+            console.log(` [.] Got ${msg.content.toString()}, corrId=${correlationId}`);
+            ch.ack(msg);
+            dbResult.status = constants.RMQ_SUCCESS;
+            dbResult.data = JSON.parse(msg.content.toString());
+            return;
+        }
+        console.log(` [.] Received corrId=${msg.properties.correlationId}, expected=${correlationId}`);
+    }
+    catch (err) {
+        dbResult.status = constants.RMQ_ERROR;
+        dbResult.data = error2;
+        console.log(err);
+    }
 }
+
 
 function receiveMessage(msg, corrId){
     if (msg.properties.correlationId === corrId){
