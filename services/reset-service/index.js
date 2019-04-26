3b69fa6ffa36ee1c37356ea0a673116aa5ddfcf1
Johnny So <jso123450@gmail.com>
2019-04-26 15:08:55 -0400

    Fixing redis session

--- services/reset-service/index.js
+++ services/reset-service/index.js
@@ -33,7 +33,7 @@ function shutdown(){
     server.close();
 }
 
-/* qa service */
+/* rest service */
 app.post('/reset', async(req, res) => {
     let endpoint = 'reset'
     let dbRes = await processRequest(req, endpoint);
@@ -62,7 +62,7 @@ const indices = ["users", "questions", "answers", "views", "q-upvotes", "a-downv
 
 async function resetDB(req){
     for (var index of indices){
-        request.post(`admin:ferdman123@130.245.169.86:92/_delete_by_query/${index}`, { "query": { "match_all": {} } });
+        request.post(`http://admin:ferdman123@130.245.169.86:92/_delete_by_query/${index}`, { json: { "query": { "match_all": {} } } });
     }
     return {status: 200, response: {status: 'OK'}};
 }
